819b914fe91b079fc2a6bde693366072e2a6b8d6
nrao <nrao200423@gmail.com>
2025-04-08 14:47:01 -0700

    preprocessing data funct completed

--- main.py
+++ main.py
@@ -8,3 +8,13 @@ from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error
 import nltk
+
+# download necessary NLTK data
+nltk.download('vader_lexicon')
+
+# preprocessing & data loading
+def preprocess_and_load_data(filepath):
+    # reads csv file from filepath and conv. 'date' column into datetime obj.
+    df = pd.read_csv(filepath)
+    df['date'] = pd.read_csv(filepath)
+    return df
